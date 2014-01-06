7f726fd62b06002c5064fc0c2b9e230b1039458e
Eduardo Stalinho <eduardooc.86@gmail.com>
2014-01-06 10:00:15 -0200

    removes invalid kwarg for urlfield

--- cas_provider/models.py
+++ cas_provider/models.py
@@ -3,16 +3,16 @@ from django.contrib.auth.models import User
 
 class ServiceTicket(models.Model):
     user = models.ForeignKey(User)
-    service = models.URLField(verify_exists=False)
+    service = models.URLField()
     ticket = models.CharField(max_length=256)
     created = models.DateTimeField(auto_now=True)
-    
+
     def __unicode__(self):
         return "%s (%s) - %s" % (self.user.username, self.service, self.created)
-        
+
 class LoginTicket(models.Model):
     ticket = models.CharField(max_length=32)
     created = models.DateTimeField(auto_now=True)
-    
+
     def __unicode__(self):
         return "%s - %s" % (self.ticket, self.created)
